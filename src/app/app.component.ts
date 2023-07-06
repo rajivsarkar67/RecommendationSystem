318f1dfee798760fb95875fbe6b18cc8bdd6a966
rajib_sarkar <rajib.sarkar@forgeahead.io>
2023-07-06 14:28:22 +0530

    integrated sample graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'RecommendationSystem';
   text_from_dropdown = 'Enter User ID : ';
   selectedTab = 'Response';
   convertedArray: any = [];
   tabArray = ['Response', 'User Chart'];
+
+  chart1: any;
 
   trialData: any = {'1. Trending Videos':
               [{'value': "Stretch Total Body 20mins with Lisa #1",'img': 'https://images.unsplash.com/photo-1562771379-eafdca7a02f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'},
@@ -29,25 +32,26 @@ export class AppComponent implements OnInit{
   newData: any = [{'Response':
                   {'1. Trending Videos':
                   [{'value': 'Beginner Burn: Cardio', 'img': 'https://thumbs.fod247.io/thumbnails/e2907877bf669b5a2e59a3824193ceab.jpg'},
-                  {'value': '30 min Total Body Apt Friendly Workout', 'img': 'https://thumbs.fod247.io/thumbnails/2b696d6fa55969f98508121e4eb6bd43.jpg'},
+                  {'value': '30 min Total Body Apt', 'img': 'https://thumbs.fod247.io/thumbnails/2b696d6fa55969f98508121e4eb6bd43.jpg'},
                   {'value': '30-Minute HIIT Strength 1:1', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'},
-                  {'value': 'R1ZE 7: CORE K1LLER', 'img': ''},
-                  {'value': '20-Minute Single Dumbbell Demolition', 'img': 'https://thumbs.fod247.io/thumbnails/0ef1ea45f2cb4eec69fac805555e8a45.jpg'},
-                  {'value': '15-Minute Abs & Core Burnout', 'img': 'https://thumbs.fod247.io/thumbnails/d64c344eb6cc53107b6bf24b84cf30f3.jpg'},
+                  {'value': 'R1ZE 7: CORE K1LLER', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'},
+                  {'value': '20-Minute Single Dumbbell', 'img': 'https://thumbs.fod247.io/thumbnails/0ef1ea45f2cb4eec69fac805555e8a45.jpg'},
+                  {'value': '15-Minute Abs & Core', 'img': 'https://thumbs.fod247.io/thumbnails/d64c344eb6cc53107b6bf24b84cf30f3.jpg'},
                   {'value': 'Beginner Burn: Core', 'img': 'https://thumbs.fod247.io/thumbnails/8a6ef6a0e8badeea277aaed21449128b.jpg'},
-                  {'value': '20-Minute Nonstop Abs & Core', 'img': ''}, {'value': 'Hike-O-Vision Pinnacles NP High Peaks Trail Three', 'img': 'https://thumbs.fod247.io/thumbnails/9aa4652d5ab9ab31e59f16383062d71a.jpg'},
-                  {'value': 'Kenta Seki: Fusion 15 - Core', 'img': 'https://thumbs.fod247.io/thumbnails/c1b8931ca47aa085449ce096565eb662.jpg'}],
+                  {'value': '20-Minute Nonstop Abs & Core', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'},
+                  {'value': 'Hike-O-Vision Pinnacles', 'img': 'https://thumbs.fod247.io/thumbnails/9aa4652d5ab9ab31e59f16383062d71a.jpg'},
+                  {'value': 'Kenta Seki: Fusion 15', 'img': 'https://thumbs.fod247.io/thumbnails/c1b8931ca47aa085449ce096565eb662.jpg'}],
                   '2. Trending Instructors':
                   [{'value': '4415,Will Brereton', 'img': 'https://thumbs.fod247.io/thumbnails/10851d57c93ce24a73dcb9403847ee4e.jpg'},
-                  {'value': '9407,Frances Flores', 'img': ''},
-                  {'value': '4649,Jillian Michaels', 'img': ''},
-                  {'value': '6762,Kenta Seki', 'img': ''},
+                  {'value': '9407,Frances Flores', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'},
+                  {'value': '4649,Jillian Michaels', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'},
+                  {'value': '6762,Kenta Seki', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'},
                   {'value': '5196,Josephine Gorchoff', 'img': 'https://thumbs.fod247.io/thumbnails/aee271c0870b002f710b3129db8fc0cb.jpg'},
-                  {'value': '9408,Gideon Akande', 'img': ''},
-                  {'value': '9380,Jaime McFaden', 'img': ''},
-                  {'value': '9362,Casey Van-Roose', 'img': ''},
-                  {'value': '9271,Jeni DelPozo', 'img': ''},
-                  {'value': '3670,Mike Donavanik', 'img': ''}],
+                  {'value': '9408,Gideon Akande', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'},
+                  {'value': '9380,Jaime McFaden', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'},
+                  {'value': '9362,Casey Van-Roose', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'},
+                  {'value': '9271,Jeni DelPozo', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'},
+                  {'value': '3670,Mike Donavanik', 'img': 'https://thumbs.fod247.io/thumbnails/93b3c0b772265809d8dbe5f03c5d808b.jpg'}],
                   '3. Trending Categories':
                   [{'value': 'Strength', 'img': 'https://images.pexels.com/photos/2827392/pexels-photo-2827392.jpeg'},
                   {'value': 'Yoga & Pilates', 'img': 'https://images.pexels.com/photos/2827392/pexels-photo-2827392.jpeg'},
@@ -72,22 +76,25 @@ export class AppComponent implements OnInit{
                   'values': [3953, 1773, 1766, 869, 711, 418, 391, 273, 188, 152]},
                   'zone_pie':
                   {'labels': ['Your Fitness Offering', 'Cox Corporate Services, LLC', 'Denver International Airport', 'JJ Keller', 'Iowa Western Community College', 'Belle Chasse YMCA', 'CareSource', 'SIRVA', 'Eskenazi Health-', 'Town of Ajax'],
+                  // {'labels': ['Your Fitness Offering', 'Cox Corporate Services, LLC', 'Denver International Airport', 'JJ Keller', 'Iowa Western Community College', 'Belle Chasse YMCA', 'CareSource', 'SIRVA', 'Eskenazi Health-'],
                   'values': [0.6581595974119339, 0.03271028037383177, 0.03199137311286844, 0.023005032350826744, 0.022645578720345075, 0.017253774263120056, 0.017253774263120056, 0.016175413371675055, 0.015815959741193385, 0.01509705248023005]},
                   'trending_pie':
                   {'labels': ['Upper Body Breakdown', 'Arms and Abs', 'Walking: 40 Min Power Walk w/ Meghan', 'SH1FT 46: LIIT', 'Move With Meaning', 'Rhythmic Reps', '30 min Compound Exercises with Low Impact Cardio', 'Getting in Tune', '30 min Plyometric Tabata Routine'],
                   'values': [0.08087706685837527, 0.04852624011502516, 0.03486700215672178, 0.02875629043853343, 0.026240115025161753, 0.02480230050323508, 0.024083393242271746, 0.02372393961179008, 0.02372393961179008]}}]}]}]
 
+  
+
   ngOnInit(): void {
-    console.log(this.newData[0]);
     for(let pair of Object.entries(this.newData[0]['Response'])){
       this.convertedArray.push({name: pair[0], values: pair[1]});
-      console.log(pair[0]);
-      console.log(pair[1]);
     }
-    console.log(this.convertedArray);
   }
+
+  ngAfterViewInit(): void {
+    this.createTrendPieChart();
+  }
+
   dropdownChanged(val: any){
-    console.log(val);
     if(val === "user"){
       this.text_from_dropdown = 'Enter User ID : ';
       this.tabArray = ['Response', 'User Chart'];
@@ -123,5 +130,190 @@ export class AppComponent implements OnInit{
     else if(val === "overall"){
       this.tabArray = ['Response','Zone Trend Chart', 'Trend Pie Chart'];
     }
+  }
+
+  generateGraph(button){
+
+    this.selectedTab = button;
+    if(button === 'Trend Pie Chart'){
+      // setTimeout(this.createTrendPieChart,1000);
+      this.createTrendPieChart();
+    }
+  }
+
+  createTrendPieChart(){
+    new Chart("trend_pie_chart_1", {
+    type: 'pie', //this denotes tha type of chart
+
+    data: {// values on X-Axis
+      labels: this.newData[0]['Graph'][0]['Pie chart'][0]['zone_pie']['labels'],
+        datasets: [{
+    label: 'My First Dataset',
+    data: this.newData[0]['Graph'][0]['Pie chart'][0]['zone_pie']['values'],
+    backgroundColor: [
+      // 'red',
+      // 'pink',
+      // 'green',
+			// 'yellow',
+      // 'orange',
+      // 'blue',
+      // 'cyan',
+      // 'black',
+      // 'gray',
+      // 'brown'	
+      'rgba(255, 99, 132, 0.75)',
+      'rgba(54, 162, 235, 0.75)',
+      'rgba(255, 206, 86, 0.75)',
+      'rgba(75, 192, 192, 0.75)',
+      'rgba(153, 102, 255, 0.75)',
+      'rgba(255, 159, 64, 0.75)',
+      'yellow',
+      'orange',
+      'cyan',
+      'gray',
+    ],
+    hoverOffset: 4
+  }],
+      },
+      options: {
+        aspectRatio:2.5
+      }
+
+    });
+
+    new Chart("trend_pie_chart_2", {
+      type: 'pie', //this denotes tha type of chart
+  
+      data: {// values on X-Axis
+        labels: this.newData[0]['Graph'][0]['Pie chart'][0]['trending_pie']['labels'],
+          datasets: [{
+      label: 'My First Dataset',
+      data: this.newData[0]['Graph'][0]['Pie chart'][0]['trending_pie']['values'],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.75)',
+        'rgba(54, 162, 235, 0.75)',
+        'rgba(255, 206, 86, 0.75)',
+        'rgba(75, 192, 192, 0.75)',
+        'rgba(153, 102, 255, 0.75)',
+        'rgba(255, 159, 64, 0.75)',
+        'yellow',
+        'orange',
+        'cyan',
+        'gray',
+      ],
+      hoverOffset: 4
+    }],
+        },
+        options: {
+          aspectRatio:2.5
+        }
+  
+    });
+
+    new Chart("trend_pie_chart_3", {
+      type: 'bar',
+      data: {
+        labels: this.newData[0]['Graph'][0]['Pie chart'][0]['top_countries_bar']['labels'],
+        datasets: [{
+          label: '# of Votes',
+          data: this.newData[0]['Graph'][0]['Pie chart'][0]['top_countries_bar']['values'],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          },
+        },
+      }
+    });
+
+    new Chart("trend_pie_chart_4", {
+      type: 'bar',
+      data: {
+        labels: this.newData[0]['Graph'][0]['Pie chart'][0]['top_category_bar']['labels'],
+        datasets: [{
+          label: '# of Votes',
+          data: this.newData[0]['Graph'][0]['Pie chart'][0]['top_category_bar']['values'],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+    new Chart("trend_pie_chart_5", {
+      type: 'bar',
+      data: {
+        labels: this.newData[0]['Graph'][0]['Pie chart'][0]['top_equipment_bar']['labels'],
+        datasets: [{
+          label: '# of Votes',
+          data: this.newData[0]['Graph'][0]['Pie chart'][0]['top_equipment_bar']['values'],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
   }
 }
