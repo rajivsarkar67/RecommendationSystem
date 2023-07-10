b7ff95a19cb684234d977e61c07d7bfb459e2cc9
Hrishikeshkanojiya <rushikanojiya1999@gmail.com>
2023-07-10 19:01:16 +0530

    added API inttegration-

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,39 +10,39 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getUserData(payload): Observable<any> {
-    return this.http.post(`http://ubuntu@54.184.178.222:5000/users_chart`, payload);
+    return this.http.post(`http://127.0.0.1:5000/users_chart`, payload);
   }
 
   getZoneData(payload): Observable<any> {
-    return this.http.post(`http://ubuntu@54.184.178.222:5000/zones_chart`, payload);
+    return this.http.post(`http://127.0.0.1:5000/zones_chart`, payload);
   }
 
   getUserZoneData(payload): Observable<any> {
-    return this.http.post(`http://ubuntu@54.184.178.222:5000/zone_user_chart`, payload);
+    return this.http.post(`http://127.0.0.1:5000/zone_user_chart`, payload);
   }
 
   getVideonameData(payload): Observable<any> {
-    return this.http.post(`http://ubuntu@54.184.178.222:5000/video_chart`, payload);
+    return this.http.post(`http://127.0.0.1:5000/video_chart`, payload);
   }
 
   getInstructorData(payload): Observable<any> {
-    return this.http.post(`http://ubuntu@54.184.178.222:5000/instructors_chart`, payload);
+    return this.http.post(`http://127.0.0.1:5000/instructors_chart`, payload);
   }
 
   getFociData(payload): Observable<any> {
-    return this.http.post(`http://ubuntu@54.184.178.222:5000/foci`, payload);
+    return this.http.post(`http://127.0.0.1:5000/foci`, payload);
   }
 
   getCategoryData(payload): Observable<any> {
-    return this.http.post(`http://ubuntu@54.184.178.222:5000/category`, payload);
+    return this.http.post(`http://127.0.0.1:5000/category`, payload);
   }
 
   getEquipmentData(payload): Observable<any> {
-    return this.http.post(`http://ubuntu@54.184.178.222:5000/equipment`, payload);
+    return this.http.post(`http://127.0.0.1:5000/equipment`, payload);
   }
 
   getOverallData(payload): Observable<any> {
-    return this.http.post(`http://ubuntu@54.184.178.222:5000/trending_videos`, payload);
+    return this.http.post(`http://127.0.0.1:5000/trending_videos`, payload);
   }
   
 }
